1f3d843d7a4b35b138151c51b27432ff603a3ad7
Furkan Kucuk <furkankucuk@outlook.com>
2024-06-16 21:50:35 +0300

    refactor: Update reconstruction_fn in RelightVDPModule
    This commit updates the reconstruction_fn method in the RelightVDPModule class. The method now performs element-wise multiplication of RGB layers with alpha layers to reconstruct the output. This change improves the clarity and functionality of the code.
    
    Refactor recent user commits:
    - Update flow_model initialization in main.py and VideoDIPDataModule
    - Remove VDPModel import from video_dip/models/__init__.py
    - Remove VDPModel from video_dip/models/vdp.py
    - Update RAFT model to use half precision
    - Update imports in video_dip/losses/__init__.py and video_dip/losses/optical_flow_warp_loss.py
    - Update VideoDIPDataModule and VideoDIPDataset
    - Add RelightVDPModule and RelightVDPModule to video_dip/models/modules
    - Add code for training RelightVDPModule with VideoDIPDataModule
    - Update .gitignore to include new directories
    - Update .gitignore and dataset.py

--- video_dip/models/modules/relight.py
+++ video_dip/models/modules/relight.py
@@ -2,6 +2,14 @@ import torch
 from . import VDPModule
 
 class RelightVDPModule(VDPModule):
+    """
+    A module for relighting in VideoDIP.
+
+    Args:
+        learning_rate (float): The learning rate for optimization (default: 1e-3).
+        loss_weights (list): The weights for different losses (default: [1, .02]).
+    """
+
     def __init__(self, learning_rate=1e-3, loss_weights=[1, .02]):
         super().__init__(learning_rate, loss_weights)
 
@@ -11,6 +19,15 @@ class RelightVDPModule(VDPModule):
         self.save_hyperparameters()
 
     def reconstruction_fn(self, rgb_output, alpha_output):
-        # Element-wise multiplication of RGB layers with alpha layers
-        return  alpha_output * rgb_output ** self.gamma_inv
+        """
+        Reconstructs the output by performing element-wise multiplication of RGB layers with alpha layers.
+
+        Args:
+            rgb_output (torch.Tensor): The RGB output tensor.
+            alpha_output (torch.Tensor): The alpha output tensor.
+
+        Returns:
+            torch.Tensor: The reconstructed output tensor.
+        """
+        return alpha_output * rgb_output ** self.gamma_inv
     

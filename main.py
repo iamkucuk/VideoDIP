a424a8cde1b4bfdfe0a1315f880302a72136e088
Furkan Kucuk <furkankucuk@outlook.com>
2024-06-16 20:41:43 +0300

    feat: Add code for training RelightVDPModule with VideoDIPDataModule
    This commit adds the necessary code to train the RelightVDPModule using the VideoDIPDataModule. It initializes the model, data module, TensorBoard logger, and trainer with the specified parameters. The model is then fitted using the trainer and data module.

---
+++ main.py
@@ -0,0 +1,27 @@
+import pytorch_lightning as pl
+from pytorch_lightning.loggers import TensorBoardLogger
+
+from video_dip.models.modules.relight import RelightVDPModule
+from video_dip.data.datamodule import VideoDIPDataModule
+from video_dip.models.optical_flow.raft import RAFT, RAFTModelSize
+
+# Initialize the model
+model = RelightVDPModule()
+
+# Initialize the data module
+data_module = VideoDIPDataModule(
+    input_path="datasets/input/pair1", 
+    target_path="datasets/GT/pair1",
+    flow_model=RAFT(RAFTModelSize.LARGE),
+    batch_size=8, 
+    num_workers=8
+)
+
+# Initialize the TensorBoard logger
+logger = TensorBoardLogger("tb_logs", name="relight_vdp")
+
+# Initialize the trainer with the logger
+trainer = pl.Trainer(logger=logger, devices=1, max_epochs=10, precision='bf16')
+
+# Fit the model
+trainer.fit(model, datamodule=data_module)
